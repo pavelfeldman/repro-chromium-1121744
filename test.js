fccf23af2b69257a73623ba461340f7b88c1dbaf
Pavel Feldman <pavel.feldman@gmail.com>
2020-08-31 12:35:58 -0700

    close context to trigger repro

--- test.js
+++ test.js
@@ -2,9 +2,10 @@ const { chromium } = require('playwright');
 
 (async() => {
   const browser = await chromium.launch({ executablePath: process.argv[2] });
-  const page = await browser.newPage();
+  const context = await browser.newContext();
+  const page = await context.newPage();
   await page.setContent('<select></select>');
   await page.click('select');
-  await page.close();
+  await context.close();
   await browser.close();
 })();
